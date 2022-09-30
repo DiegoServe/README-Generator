8d3697a799a1ed77a712f18dbe2bc5123aab1f9b
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2022-09-30 19:51:43 -0400

    fix

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,32 +4,24 @@ function generateMarkdown(data) {
   
 ## Licensing:
 [![license](https://img.shields.io/badge/license-${data.licensing}-blue)](https://shields.io)
-
 ## Table of Contents
 -[Description](#description)
 -[Installation](#installation)
 -[Usage](#usage)
 -[Contribution](#contribution)
 -[Additional Information](#additional-information)
-
 ## Description:
 ${data.description}
-
 ## Installation:
 ${data.installation}
-
 ## Usage:
 ${data.usage}
-
 ## License:
 ${data.license}
-
 ## Contribution:
 ${data.contribute}
-
 ## Testing:
 ${data.test}
-
 ## Aditional Information:
 - Github: [${data.githubUsername}](https://github.com/${data.githubUsername})
 - Email: ${data.email} `;
