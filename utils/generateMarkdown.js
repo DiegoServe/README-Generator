730025c04a8c565d5be7c9925d25bcb9328cacb0
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2022-09-30 17:20:35 -0400

    table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+const generateMarkdown(data) {
+  return # ${data.title}
+}
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
-
 // TODO: Create a function that returns the license link
+## Table of Contents
+-[Description] (#description)
+-[Installation] (#installation)
+-[Usage] (#usage)
+-[Contribution] (#contribution)
+-[Testing] (#testing)
+-[Additional Info] (#additional-info)
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+;
 }
 
 module.exports = generateMarkdown;
